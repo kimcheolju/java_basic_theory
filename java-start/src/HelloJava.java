4e1645495b05c8e3fc9dfcdae25760969c2cb44f
kimcj020828m <kimcj020828@gmail.com>
2024-04-15 14:10:22 +0900

    김영한 - 자바 입문 기초
    [섹션1]
    - 환경설정
    - 주석
    - 자바란

---
+++ java-start/src/HelloJava.java
@@ -0,0 +1,24 @@
+/*
+public class HelloJava
+    'HelloJava'는 Class다
+    HelloJava.java 파일을 만들었다.
+    여기서 파일명과 Class명은 같아야 한다.
+    Class는 블록'{}'으로 시작과 끝을 나타낸다.
+
+public static void main(String[] args)
+    main 메서드라 한다.
+    자바는 main(String[] args) 메서드를 찾아서 프로그램을 실행한다.
+    지금은 단순히 main은 프로그램의 시작점으로 이해하면 된다.
+
+System.out.println("hello java");
+     System.out.println(); 값을 콘솔에 출력하는 기능이다.
+     "hello java": 자바는 문자열을 사용할 때 "(쌍따옴표)를 사용한다.
+     ;: 자바는 세미콜론으로 문장을 구분한다. 문장이 끝나면 세미콜론을 필수로 넣어주어야 한다.
+
+보통 블록의 하위는 내용을 구분짓기 위해 스페이스 4번을 입력한다.
+ */
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("hello java");
+    }
+}
